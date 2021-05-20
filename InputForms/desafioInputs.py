45ec00bbeef6312cf25df91f37b395af4209f243
Ramon Lobo <ramonlobo633@gmail.com>
2021-05-20 01:28:31 -0300

    Renomeando algumas variáveis

--- InputForms/desafioInputs.py
+++ InputForms/desafioInputs.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 import pandas as pd
 
 
-dados = pd.read_excel('challenge.xlsx')
+df_challenge = pd.read_excel('challenge.xlsx')
 
 driver = webdriver.Firefox(executable_path='C:/tools/selenium/geckodriver.exe')
 driver.get('http://www.rpachallenge.com')
@@ -14,25 +14,25 @@ StartButton.click()
 for i in range(10):
 
     FirstNameInput= driver.find_element_by_xpath("//*[contains(text(),'First Name')]//following-sibling::input")
-    FirstNameInput.send_keys(dados.loc[i, :][0])
+    FirstNameInput.send_keys(df_challenge.loc[i, :][0])
 
     LastNameInput= driver.find_element_by_xpath("//*[contains(text(),'Last Name')]//following-sibling::input")
-    LastNameInput.send_keys(dados.loc[i, :][1])
+    LastNameInput.send_keys(df_challenge.loc[i, :][1])
 
     CompanyNameInput= driver.find_element_by_xpath("//*[contains(text(),'Company Name')]//following-sibling::input")
-    CompanyNameInput.send_keys(dados.loc[i, :][2])
+    CompanyNameInput.send_keys(df_challenge.loc[i, :][2])
 
     RoleNameInput= driver.find_element_by_xpath("//*[contains(text(),'Role in Company')]//following-sibling::input")
-    RoleNameInput.send_keys(dados.loc[i, :][3])
+    RoleNameInput.send_keys(df_challenge.loc[i, :][3])
 
     AddressNameInput= driver.find_element_by_xpath("//*[contains(text(),'Address')]//following-sibling::input")
-    AddressNameInput.send_keys(dados.loc[i, :][4])
+    AddressNameInput.send_keys(df_challenge.loc[i, :][4])
 
     EmailInput= driver.find_element_by_xpath("//*[contains(text(),'Email')]//following-sibling::input")
-    EmailInput.send_keys(dados.loc[i, :][5])
+    EmailInput.send_keys(df_challenge.loc[i, :][5])
 
     PhoneInput= driver.find_element_by_xpath("//*[contains(text(),'Phone Number')]//following-sibling::input")
-    PhoneInput.send_keys(int(dados.loc[i, :][6]))
+    PhoneInput.send_keys(int(df_challenge.loc[i, :][6]))
 
     SubmitButton = driver.find_element_by_xpath("/html/body/app-root/div[2]/app-rpa1/div/div[2]/form/input")
     SubmitButton.click()
